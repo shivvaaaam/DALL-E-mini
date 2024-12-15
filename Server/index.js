b13a1ae06653b9f8db4e3ab6a996fbe44a98081a
shivvaaaam <shivrajmahi123@gmail.com>
2024-12-15 15:26:30 +0530

    Redeploy backend to Render with latest updates

--- Server/index.js
+++ Server/index.js
@@ -11,12 +11,17 @@ import cors from 'cors'
 
 const app = express();
 const corsOptions = {
-  origin: ['https://dall-e-mini-dl1e-git-main-shivam-guptas-projects-f99d138a.vercel.app'], // Local and deployed frontend
+  origin: ['https://dall-e-mini-dl1e-git-main-shivam-guptas-projects-f99d138a.vercel.app'], // Array of allowed origins
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true, // Include if using cookies or sessions
 };
+
 app.use(cors(corsOptions));
+
+
+app.use(cors(corsOptions));
+
 
 
 
